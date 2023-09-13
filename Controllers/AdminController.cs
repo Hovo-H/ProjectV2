aee0ee760ba67794accdf7ca1858c4d8d8f19720
Asus <Asus@DESKTOP-8L721MH>
2023-09-13 17:37:07 +0400

    Добавьте файлы проекта.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Data.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MyShopDataContext _context;
+        public AdminController(MyShopDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddUpdateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddUpdateUser(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+            return View(user);
+        }
+    }
+}
